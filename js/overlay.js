3fc6d20643dae09bb94b88c8a9b30f12ee9184f6
Dan Butler <Dan Butler>
2017-03-09 02:48:13 +0000

    Sorted out media queries, rebuilding the css mobile first

--- js/overlay.js
+++ js/overlay.js
@@ -1,7 +1,34 @@
-function openNav() {
+function openNav1() {
     document.getElementById("loc1").style.width = "100%";
 }
 
-function closeNav() {
+function openNav2() {
+    document.getElementById("loc2").style.width = "100%";
+}
+
+function openNav3() {
+    document.getElementById("loc3").style.width = "100%";
+}
+
+function openNav4() {
+    document.getElementById("loc4").style.width = "100%";
+}
+
+
+
+
+function closeNav1() {
     document.getElementById("loc1").style.width = "0%";
+}
+
+function closeNav2() {
+    document.getElementById("loc2").style.width = "0%";
+}
+
+function closeNav3() {
+    document.getElementById("loc3").style.width = "0%";
+}
+
+function closeNav4() {
+    document.getElementById("loc4").style.width = "0%";
 }
